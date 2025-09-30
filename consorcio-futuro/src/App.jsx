fc1c90e046f44dafe507b8c51bc10337096781a3
ElPerroGCP <emilio.abarca.rail@gmail.com>
2025-09-30 05:29:21 -0300

    carpeta buena

---
+++ consorcio-futuro/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 Consorcio FUTURO</h1>
+      <p>Tu proyecto React con Vite está funcionando.</p>
+    </div>
+  );
+}
+
+export default App;
